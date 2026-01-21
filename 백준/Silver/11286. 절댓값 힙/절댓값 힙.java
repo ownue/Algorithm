d920049645e0213d3ae436f4e65ef8b24455fe40
eunwoo <enu_i@naver.com>
2026-01-21 17:49:32 +0900

    [Silver I] Title: 절댓값 힙, Time: 244 ms, Memory: 25712 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            int absA = Math.abs(a);
+            int absB = Math.abs(b);
+
+            if (absA != absB) {
+                return absA - absB; // 절댓값 작은 게 우선
+            }
+            return a - b;   // 절댓값이 동일하다면 실제 값이 작은 게 우선
+        });
+        sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (pq.isEmpty()) sb.append(0).append("\n");
+                else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else pq.offer(x);
+        }
+        System.out.println(sb);
+    }
+}
